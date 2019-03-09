ae425a5817a57672e15af6c2d97ea345a055c4a0
Corey Doss <cjdw94@mail.umkc.edu>
2019-03-09 10:21:33 -0600

    Duplicate First Commit In Separate Branch

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-// ------------ Begin - The block of the code -----------------
 const fs =  require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
-const DataFile = require('./data.js');
-// ------------ End - The block of the code -----------------
+const customers = require('./customer.js');
 
-// ------------ Begin - command configuration -----------------
 const customerIDopt = {
     describe: 'Customer ID number',
     demand : true,
@@ -18,34 +15,28 @@ const customerNameopt = {
     alias : 'n'
 }
 
-const customerEmailopt  = {
-    describe: 'Customer Email',
+const customerEmailopt = {
+    describe: 'Customer email address',
     demand : true,
     alias : 'e'
 }
 
-const customerPhoneopt  = {
+const customerPhoneopt = {
     describe: 'Customer phone number',
     demand : true,
     alias : 'p'
 }
 
-
 const argv =  yargs
+
     .command('add','Add a new customer entry',{
       customerID: customerIDopt,
       customerName: customerNameopt,
 	  customerEmail: customerEmailopt,
-      customerPhone: customerPhoneopt
+	  customerPhone: customerPhoneopt
     })
-	.command('update','Update a customer entry',{
-      customerID: customerIDopt,
-      customerName: customerNameopt,
-	  customerEmail: customerEmailopt,
-      customerPhone: customerPhoneopt
-    })
-    .command('list','List all customers entries')
-    .command('read','Read a specific customer information',{
+    .command('list','List all customer entries')
+    .command('read','Retrieve a customer',{
       customerID: customerIDopt
     })
     .command('remove','Remove a customer',{
@@ -53,61 +44,20 @@ const argv =  yargs
     })
     .help()
     .argv;
-// ------------ End - command configuration -----------------
 
 var command = argv._[0];
 
-// ------------ Begin - Adding -----------------
 if (command === 'add'){
-    var info = DataFile.addData(argv.customerID,argv.customerName,argv.customerEmail,argv.customerPhone);
-    if (info){
-      DataFile.logData(info); 
+    var customer = customers.addCustomer(argv.customerID, argv.customerName, argv.customerEmail, argv.customerPhone);
+    if (customer){
+      customers.logCustomer(customer);                                //add a new customer
     } else{
-      console.log("Data already exists");
+      console.log("Customer already exists");
     }
-}
-// ------------ End - Adding -----------------
-
-// ------------ Begin - Updating -----------------
-else if (command === 'update'){
-
-}
-// ------------ End - Updating -----------------
-
-// ------------ Begin - listing -----------------
-else if (command === 'list') {
-  var AllData = DataFile.getAll();
-    console.log(`Printing ${AllData.length} info(s).`);
-    AllData.forEach((info)=>{
-    DataFile.logData(info);
+} else if (command === 'list') {
+  var AllCustomers = customers.getAll();
+  console.log(`Printing ${AllCustomers.length} customer(s).`);
+  AllCustomers.forEach((customer)=>{                                //list all customer(s)
+    customers.logCustomer(customer);
   });
-}
-// ------------ End - listing -----------------
-
-// ------------ Begin - Reading -----------------
-else if (command === 'read') {
-   var info = DataFile.readData(argv.customerID);
-   if(info){
-    DataFile.logData(info);      
-          }
-   else{
-    console.log("Data not found");
-   }
-}
-// ------------ End - Reading -----------------
-
-// ------------ Begin - Deleting -----------------
-else if (command === 'remove') {
-   var info = DataFile.removeData(argv.customerID);
-   if(info){
-    console.log(`This ID (${argv.customerID}) was deleted successfully`);
-          }
-   else{
-    console.log("Data not found");
-   }
-}
-// ------------ End - Deleting -----------------
-
-else{
-  console.log('Please type the right command'); 
 }
